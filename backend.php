1752c3d3e933e7f6fa1e3dcd8e5bfa0ccf84a890
JaimeAguiAro <jymi91@gmail.com>
2021-10-30 17:24:31 +0200

    Boostrap integrado

--- backend.php
+++ backend.php
@@ -7,11 +7,11 @@
                 WHERE especializacion = '$espec' 
                 ORDER BY puntuacion DESC LIMIT 3";
         $result = mysqli_query($conection,$sql);
-        echo "<br/><table>";
-        echo "<tr><th>Personaje</th><th>Clase</th><th>Puntuacion</th></tr>";
+        echo "<br/><table class='table table-dark table-striped table-sm'>";
+        echo "<tr class='text-center'><th scope='col'>Personaje</th><th scope='col'>Clase</th><th scope='col'>Puntuacion</th></tr>";
         while ($row = mysqli_fetch_array($result)) {
-            echo "<tr>";
-            echo "<td>".$row["nombre"]."</td>";
+            echo "<tr class='text-center'>";
+            echo "<td scope='row'>".$row["nombre"]."</td>";
             echo "<td>".$row["clase"]."</td>";
             echo "<td>".$row["puntuacion"]."</td>";
             echo "</tr>";
@@ -27,11 +27,11 @@
                 FROM grupo
                 ORDER BY puntuacion DESC LIMIT 3";
         $result = mysqli_query($conection,$sql);
-        echo "<br/><table>";
-        echo "<tr><th>Grupo</th><th>Puntuacion</th></tr>";
+        echo "<br/><table class='table table-dark table-striped'>";
+        echo "<tr><th scope='col'>Grupo</th><th scope='col'>Puntuacion</th></tr>";
         while ($row = mysqli_fetch_array($result)) {
             echo "<tr>";
-            echo "<td>".$row["nombre"]."</td>";
+            echo "<td scope='row'>".$row["nombre"]."</td>";
             echo "<td>".$row["puntuacion"]."</td>";
             echo "</tr>";
         }
@@ -42,15 +42,15 @@
     function GetTopHermandades(){
         $conection = mysqli_connect('127.0.0.1', 'root', '');
         mysqli_select_db($conection, "dungeonrio");
-        $sql = "SELECT nombre, clase, puntuacion FROM personaje WHERE especializacion = '$espec' ORDER BY puntuacion DESC LIMIT 3";
+        $sql = "SELECT nombre,avance 
+                FROM hermandad ORDER BY avance DESC LIMIT 5";
         $result = mysqli_query($conection,$sql);
-        echo "<br/><table>";
-        echo "<tr><th>Personaje</th><th>Clase</th><th>Puntuacion</th></tr>";
+        echo "<br/><table class='table table-dark table-striped'>";
+        echo "<tr><th scope='col'>Nombre</th><th scope='col'>Avance</th></tr>";
         while ($row = mysqli_fetch_array($result)) {
             echo "<tr>";
-            echo "<td>".$row["nombre"]."</td>";
-            echo "<td>".$row["clase"]."</td>";
-            echo "<td>".$row["puntuacion"]."</td>";
+            echo "<td scope='row'>".$row["nombre"]."</td>";
+            echo "<td>".$row["avance"]."</td>";
             echo "</tr>";
         }
         echo "</table>";
--- index.php
+++ index.php
@@ -5,21 +5,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pagina Inicial</title>
-    <style>
-        table, th, td {
-            border: 1px solid black;
-            border-collapse: collapse;
-        }
-    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <?php
     include "backend.php";
 ?>
 <body>
-    <h1>HEY HEY PEOPLE</h1>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <header class="container">
+        <h1>Dungeonrio</h1>
+    </header>
+    <body class="container">
+        <div class="container">
+            <div class="row">
+                <div class="col-5">
+                    <div class="card">
+                        <h1 class="card-header">Tanks</h1>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+                            <?php
+                            GetTopPersonajes("Tank");
+                            ?>
+                            </blockquote>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     <?php
-        GetTopPersonajes("Tank");
+        GetTopPersonajes("DPS");
+        GetTopPersonajes("Healer");
         GetTopGrupos();
+        GetTopHermandades();
     ?>
+    </body>
+    <footer>
+
+    </footer>
 </body>
 </html>
