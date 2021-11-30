31d40e84011f56946211598b9dac9de545bd441b
JaimeAguiAro <jymi91@gmail.com>
2021-11-30 19:08:35 +0100

    prueba de login 2

--- login.php
+++ login.php
@@ -38,7 +38,7 @@
                 mysqli_close($conection);
                 $_SESSION['user'] = $row["usuario"];
                 $_SESSION['id'] = $id;
-                header("Location: index.php");
+                header("Location: https://dungeonrio.azurewebsites.net/");
             }else {
                 mysqli_free_result($result);
                 mysqli_close($conection);
