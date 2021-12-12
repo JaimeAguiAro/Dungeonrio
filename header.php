307c2873168ddc63e9e5eb26462ab600ba8bda71
JaimeAguiAro <jymi91@gmail.com>
2021-12-12 20:30:31 +0100

    dropdown arreglado

--- header.php
+++ header.php
@@ -41,6 +41,21 @@
             }
         }).change();
     });
+    $(document).ready(function(){
+        dropdown();
+    });
+    $( window ).resize(function() {
+        dropdown();
+    });
+    function dropdown(){
+        if ($( window ).width() <= 992) {
+            $("#userLog").removeClass("dropstart");
+            $("#userLog").addClass("dropdown");
+        }else{
+            $("#userLog").removeClass("dropdown");
+            $("#userLog").addClass("dropstart");
+        }
+    }
 </script>  
 <header class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
     <div class="container-fluid">
@@ -79,13 +94,13 @@
         <input style="width: 270px;" class="form-control" type="text" id="busqueda" placeholder="Buscar Personaje o hermandad">
         <?php
             if (isset($_SESSION['user'])) {
-                echo "<div class='dropstart ps-2'>";
+                echo "<div id='userLog' class='dropstart ps-2'>";
                 echo "<button type='button' class='btn btn-secondary dropdown-toggle' data-bs-toggle='dropdown' aria-expanded='false'>".$_SESSION['user']."</button>
-                    <ul class='dropdown-menu'>
-                        <li><a class='dropdown-item' href='configuracion.php'>Configuracion</a></li>
-                        <li><hr class='dropdown-divider'></li>
-                        <li><a class='dropdown-item' id='cerrarSesion' style='cursor: pointer;'>Cerrar Sesion</a></li>
-                    </ul>";
+                        <ul class='dropdown-menu'>
+                            <li><a class='dropdown-item' href='configuracion.php'>Configuracion</a></li>
+                            <li><hr class='dropdown-divider'></li>
+                            <li><a class='dropdown-item' id='cerrarSesion' style='cursor: pointer;'>Cerrar Sesion</a></li>
+                        </ul>";
                 echo "</div>";
             }else {
                 echo "<a class='m-1 btn btn-dark' href='login.php'>Login</a>";
